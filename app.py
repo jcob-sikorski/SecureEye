b8a643840ba494926b2178b3d7ca2a95934dbcb4
jcob-sikorski <jm.siekiera@gmail.com>
2023-05-20 14:33:35 +0200

    added secrets and webhook code

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Flask heroku app.'
+
+# if __name__ == '__main__':
+#     app.run()
+
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Flask heroku app.'
+VERIFY_TOKEN = os.getenv('VERIFY_TOKEN') # Replace this with your verify token
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/messaging-webhook', methods=['GET'])
+def verify_webhook():
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+
+    if mode and token:
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return challenge
+        else:
+            abort(403)  # Forbidden
+
+if __name__ == "__main__":
+    configure()
+    app.run(port=5000, debug=True)
