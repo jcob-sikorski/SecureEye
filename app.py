750676541c79a3009fcf870111d26206d6a0dc1f
jcob-sikorski <jm.siekiera@gmail.com>
2023-05-21 20:00:51 +0200

    Added S3 bucket functionality - fixed bugs.

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
 S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
 
 
-boto3.setup_default_session(aws_access_key_id='S3_ACCESS_KEY',
-                            aws_secret_access_key='S3_SECRET_ACCESS_KEY',
+boto3.setup_default_session(aws_access_key_id=S3_ACCESS_KEY,
+                            aws_secret_access_key=S3_SECRET_ACCESS_KEY,
                             region_name='eu-west-1')
 
 
