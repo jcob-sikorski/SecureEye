5a4a32d6d50c2a56f5157217b94137a12fdef01f
jcob-sikorski <jm.siekiera@gmail.com>
2023-05-23 15:22:59 +0200

    Server should now receive the image sent to /upload, store it in a AWS S3 bucket and send the url to it to my Messenger.

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ import os, json, requests
 import boto3
 from werkzeug.utils import secure_filename
 
-# TODO add new route for /upload to aws s3 bucket
-# camera sends image to /upload which sends image to s3 bucket
-# and flask app sends image url to facebook messenger
+# TODO establish connection with messenger by webhook but retrieve messages when someone sends image to /upload and then send them to messenger user specified by pasted URL
 
 def configure_secrets():
     load_dotenv()
-
 
 configure_secrets()
 
@@ -19,11 +16,9 @@ app = Flask(__name__)
 S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
 S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
 
-
 boto3.setup_default_session(aws_access_key_id=S3_ACCESS_KEY,
                             aws_secret_access_key=S3_SECRET_ACCESS_KEY,
                             region_name='eu-west-1')
-
 
 s3 = boto3.client('s3')
 
@@ -54,32 +49,17 @@ def sendResponseToMessenger(sender_psid, response):
     print(r.text)
 
 
-def handleMessageFromUser(sender_psid, received_message):
-    if 'text' in received_message:
-        image_url = "https://www.usaoncanvas.com/images/low_res_image.jpg"
+UPLOAD_SECRET_KEY = os.getenv('UPLOAD_SECRET_KEY')
 
-        response = {
-            'attachment': {
-                'type': 'image',
-                'payload': {
-                    'url': image_url,
-                    'is_reusable': True
-                }
-            }
-        }
 
-        sendResponseToMessenger(sender_psid, response)
-    else:
-        response = {
-            'text': f"This app only accepts text messages."
-        }
-
-        sendResponseToMessenger(sender_psid, response)
-
-# TODO make it secure so only camera can upload to s3 bucket
-# e.g. add a secret key to the request
 @app.route('/upload', methods=['POST'])
 def uploadImageToS3():
+    # Check if the secret key is correct
+    provided_secret_key = request.headers.get('UPLOAD-SECRET-KEY')
+
+    if provided_secret_key != UPLOAD_SECRET_KEY:
+        return 'Invalid secret key', 403
+    
     if 'file' not in request.files:
         return 'No file part', 400
 
@@ -90,14 +70,34 @@ def uploadImageToS3():
     if file:
         filename = secure_filename(file.filename)
         s3.upload_fileobj(file, 'images-for-messenger', filename)
+
+        # Create a presigned URL for the uploaded file
+        image_url = s3.generate_presigned_url('get_object',
+                                                  Params={'Bucket': 'images-for-messenger',
+                                                          'Key': filename},
+                                                  ExpiresIn=3600)
+
+        # Send the presigned URL to the image on S3 bucket to Facebook Messenger User
+        response = {
+            'attachment': {
+                'type': 'image',
+                'payload': {
+                    'url': image_url,
+                    'is_reusable': True
+                }
+            }
+        }
+
+        sender_psid = "100023847571943"
+        sendResponseToMessenger(sender_psid, response)
+
         return 'File uploaded successfully', 200
 
 
-# TODO do we actually upload this to heroku?
-VERIFY_TOKEN = os.getenv('VERIFY_TOKEN') # Replace this with your verify token
+VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['GET'])
 def webhook():
     if request.method == 'GET':
 
@@ -117,9 +117,7 @@ def webhook():
 
             if mode == 'subscribe' and token == VERIFY_TOKEN:
                 print('WEBHOOK_VERIFIED')
-
                 challenge = request.args.get('hub.challenge')
-
                 return challenge, 200
             else:
                 return 'ERROR', 403
@@ -127,52 +125,5 @@ def webhook():
         return 'SOMETHING', 200
 
 
-    if request.method == 'POST':
-
-        if 'hub.mode' in request.args:
-            mode = request.args.get('hub.mode')
-            print(mode)
-        if 'hub.verify_token' in request.args:
-            token = request.args.get('hub.verify_token')
-            print(token)
-        if 'hub.challenge' in request.args:
-            challenge = request.args.get('hub.challenge')
-            print(challenge)
-
-        if 'hub.mode' in request.args and 'hub.verify_token' in request.args:
-            mode = request.args.get('hub.mode')
-            token = request.args.get('hub.verify_token')
-
-            if mode == 'subscribe' and token == VERIFY_TOKEN:  
-                print('WEBHOOK_VERIFIED')
-
-                challenge = request.args.get('hub.challenge')
-
-                return challenge, 200
-            else:
-                return 'ERROR', 403
-            
-
-        data = request.data
-        body = json.loads(data.decode('utf-8'))
-
-        if 'object' in body and body['object'] == 'page':
-            entries = body['entry']
-
-            for entry in entries:
-                webhook_event = entry['messaging'][0]
-                print(webhook_event)
-
-                sender_psid = webhook_event['sender']['id']
-                print(f'Sender PSID: {sender_psid}')
-
-                if 'message' in webhook_event:
-                    handleMessageFromUser(sender_psid, webhook_event['message'])
-
-                return 'EVENT_RECEIVED', 200
-        else:
-            return 'ERROR', 404
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
