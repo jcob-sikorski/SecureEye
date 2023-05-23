6b4475d81be5399080bd6b275978fff1216433e0
jcob-sikorski <jm.siekiera@gmail.com>
2023-05-23 19:55:59 +0200

    now accepting raw bytes from camera, converting them to image and sending to s3 bucket

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 from dotenv import load_dotenv
-import os, json, requests
+import os, requests
 import boto3
-from werkzeug.utils import secure_filename
+from PIL import Image
+import io
 
 def configure_secrets():
     load_dotenv()
@@ -47,46 +48,45 @@ def sendResponseToMessenger(sender_psid, response):
     print(r.text)
 
 
-UPLOAD_SECRET_KEY = os.getenv('UPLOAD_SECRET_KEY')
-
-
 @app.route('/upload', methods=['POST'])
 def uploadImageToS3():
-    if 'file' not in request.files:
-        return 'No file part', 400
+    image_raw_bytes = request.get_data() 
 
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file', 400
+    # Convert raw bytes into Image object
+    image = Image.open(io.BytesIO(image_raw_bytes))
 
-    if file:
-        filename = secure_filename(file.filename)
+    # Convert image into .png format
+    image.save("temp.png")
 
-        # Define S3 resource instead of client to use the upload_file method
-        s3 = boto3.resource('s3')
-        s3.Bucket('images-for-messenger').put_object(Key=filename, Body=file)
+    # Open the saved image file in read bytes mode
+    file = open("temp.png", "rb")
 
-        # Create a URL for the uploaded file
-        image_url = f"https://images-for-messenger.s3.eu-west-1.amazonaws.com/{filename}"
-        print(image_url)
-        
-        # Send the URL to the image on S3 bucket to Facebook Messenger User
-        response = {
-            'attachment': {
-                'type': 'image',
-                'payload': {
-                    'url': image_url,
-                    'is_reusable': True
-                }
+    # Define S3 resource instead of client to use the upload_file method
+    s3 = boto3.resource('s3')
+    s3.Bucket('images-for-messenger').put_object(Key="temp.png", Body=file)
+
+    # Create a URL for the uploaded file
+    image_url = f"https://images-for-messenger.s3.eu-west-1.amazonaws.com/temp.png"
+    print(image_url)
+
+    # Send the URL to the image on S3 bucket to Facebook Messenger User
+    response = {
+        'attachment': {
+            'type': 'image',
+            'payload': {
+                'url': image_url,
+                'is_reusable': True
             }
         }
+    }
 
-        sender_psid = "5930112510450871"
-        sendResponseToMessenger(sender_psid, response)
+    sender_psid = "5930112510450871"
+    sendResponseToMessenger(sender_psid, response)
 
-        return 'File uploaded successfully', 200
+    file.close()  # Ensure to close the file after upload
+    os.remove("temp.png")  # Remove the local temporary file
 
-
+    return 'File uploaded successfully', 200
 
 VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 
